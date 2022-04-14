26819df93b82c55cf46ed14fdc9f846d3c87b964
wormyn <87888767+wormyn@users.noreply.github.com>
2022-04-14 16:49:59 -0700

    actions server runs

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const server = express();
 const projectsRouter = require('./projects/projects-router')
+const actionsRouter = require('./actions/actions-router')
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
@@ -9,6 +10,7 @@ const projectsRouter = require('./projects/projects-router')
 
 server.use(express.json())
 server.use('/api/projects', projectsRouter)
+server.use('/api/actions', actionsRouter)
 
 server.get('/', (req, res) => {
     res.send('<h1>running</h1>');
